33b021a056bac94c6b0f6cd08fa489467b9f8141
madelmann <no-reply@github.com>
2026-01-11 23:55:12 +0100

    refactored CMake build to use SLANG_WORKSPACE_ROOT instead of manually entering the slang base path

---
+++ CMake/Dependencies.cmake
@@ -0,0 +1,12 @@
+
+# Slang base
+SET( EXTERNALS "${SLANG_WORKSPACE_ROOT}/externals" )
+
+# Slang library includes
+SET( BUILD_SLANG_INC "${SLANG_WORKSPACE_ROOT}/slang/src/Projects/Core" )
+SET( BUILD_SLANG_LIB "${SLANG_WORKSPACE_ROOT}/slang/build/lib" )
+
+# JSON library includes
+SET( JSON_INCLUDE_DIR "/usr/include/jsoncpp" )
+SET( JSON_LIBRARY "/usr/lib" )
+
--- Extension/Extension.cpp
+++ Extension/Extension.cpp
@@ -17,7 +17,7 @@ namespace Json {
 
 
 Extension::Extension()
-: Extensions::AExtension( "extJson", "0.3.0" )
+: Extensions::AExtension( "extJson", "0.3.1" )
 {
     mName = "extJson (using libjsoncpp " + std::string( JSONCPP_VERSION_STRING ) + ")";
 }
